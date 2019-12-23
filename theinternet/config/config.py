8a5b552cc9201ee45bbdd0e8ec4f7967860989fc
Dariusz Piatek <dariusz.piatek@pmi.com>
2019-12-23 16:32:58 +0100

    checkbox assertions fix

--- theinternet/config/config.py
+++ theinternet/config/config.py
@@ -1,4 +1,11 @@
+import platform
+
+os_name = platform.system()
+
 base_url="https://the-internet.herokuapp.com/"
 implicit_timeout=10
 explicit_timeout=10
-chromedriver_path="../driver/chromedriver"
+if os_name == "Windows":
+    chromedriver_path="../driver/windows/chromedriver"
+if os_name == "Linux":
+    chromedriver_path = "../driver/linux/chromedriver"
